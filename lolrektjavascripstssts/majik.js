0c69dc983a0fb58a34b5ed75c126becc2f1515fc
unknown <androidnoob@WS-I3-33.WS-ACADBUS.EDU>
2016-05-26 07:53:50 -0700

    lol fam update

--- lolrektjavascripstssts/majik.js
+++ lolrektjavascripstssts/majik.js
@@ -1,5 +1,5 @@
 function sumThings() {
-  var nums = prompt("Enter sum nums m9: ", "put sum nums here bruh");
+  var nums = prompt("Enter sum nums m9: ", "put sum nums here bruh".split(","));
   for(var i = 0; i < nums.length; i++){
     
   }
